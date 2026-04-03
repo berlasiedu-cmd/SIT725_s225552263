98ed0141257b0dfc1472b50e2cd789acf312c364
berlasiedu <berl.asiedu@gmail.com>
2026-04-03 21:50:16 +1100

    Added MongoDB

--- 3.2P/server.js
+++ 3.2P/server.js
@@ -1,6 +1,17 @@
-const express = require('express');
-const app = express();
-const PORT = 3000;
+// API endpoint
+
+const express = require("express")
+const mongoose = require('mongoose');
+
+const app = express()
+const port = process.env.port || 3004
+
+
+// Middleware
+app.use(express.static(__dirname + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 
 // sample data
 const recipes = [
@@ -37,15 +48,56 @@ const recipes = [
 ];
 
 
-// API endpoint
-app.get('/api/recipes', (req, res) => {
+app.get("/recipes", (req, res) => {
   res.json(recipes);
 });
 
-// serve frontend
-app.use(express.static('client'));
 
-// START SERVER (THIS IS VERY IMPORTANT)
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+mongoose.connect('mongodb://127.0.0.1:27017/myprojectDB')
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
+
+
+// 2. Define your schema and model
+const ProjectSchema = new mongoose.Schema({
+title: String,
+image: String,
+link: String,
+description: String,
 });
+
+const Project = mongoose.model('Project', ProjectSchema);
+
+const UserSchema = new mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String,
+  confpassword: String
+});
+
+const User = mongoose.model("User", UserSchema);
+
+// 3. REST API route
+app.get('/api/projects', async (req, res) => {
+const projects = await Project.find({});
+res.json({ statusCode: 200, data: projects, message: 'Success' });
+});
+
+app.post("/signup", async (req, res) => {
+  try {
+    const user = new User(req.body);
+    await user.save();
+    res.json({ message: "User saved!" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+
+app.listen(port, () => {
+console.log(`App listening on port ${port}`);
+});
+
+
+
